1ada364fe33300288f9fc5fd3df6fed278da4be5
Nastasja Klim <nastjam2002@gmail.com>
2025-10-19 17:49:40 +0300

    Update loan-decision.spec.ts
    исправила ошибки Prettier (2)

--- tests/loan-decision.spec.ts
+++ tests/loan-decision.spec.ts
@@ -4,80 +4,80 @@ import { LoanDto } from './dto/loan-dto'
 
 const URL = 'https://backend.tallinn-learning.ee/api/loan-calc/decision'
 
-test('positive decision / low risk - 200 OK', async ({ request })=> {
+test('positive decision / low risk - 200 OK', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
     data: LoanDto.lowRisk(),
-  });
+  })
 
-  const body = await response.json();
+  const body = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(body.riskLevel).toBe('Low Risk');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(body.riskLevel).toBe('Low Risk')
 })
 
-test('positive decision / medium risk - 200 OK', async ({ request })=> {
+test('positive decision / medium risk - 200 OK', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
     data: LoanDto.mediumRisk(),
-  });
+  })
 
-  const body = await response.json();
+  const body = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(body.riskLevel).toBe('Medium Risk');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(body.riskLevel).toBe('Medium Risk')
 })
 
-test('negative decision / very high risk - 200 OK', async ({ request })=> {
+test('negative decision / very high risk - 200 OK', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
     data: LoanDto.veryHighRiskNegative(),
-  });
+  })
 
-  const body = await response.json();
+  const body = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(body.riskLevel).toBe('Very High Risk');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(body.riskLevel).toBe('Very High Risk')
 })
 
-test('positive decision / high risk - 200 OK', async ({ request })=> {
+test('positive decision / high risk - 200 OK', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
     data: LoanDto.highRisk(),
-  });
+  })
 
-  const body = await response.json();
+  const body = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(body.riskLevel).toBe('High Risk');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(body.riskLevel).toBe('High Risk')
 })
 
-test('valid JSON / boundary values - 200 OK', async ({ request })=> {
+test('valid JSON / boundary values - 200 OK', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
-    data: LoanDto.boundaryMinValid()
-  });
+    data: LoanDto.boundaryMinValid(),
+  })
 
-  const body = await response.json();
+  const body = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(body.riskLevel).toBe('Very High Risk');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(body.riskLevel).toBe('Very High Risk')
 })
 
-test('income 0 - 400 bad request', async ({ request })=> {
+test('income 0 - 400 bad request', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
-    data: LoanDto.incomeZero()
-  });
+    data: LoanDto.incomeZero(),
+  })
 
-  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST);
+  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-test('negative debt - 400 bad request', async ({ request })=> {
+test('negative debt - 400 bad request', async ({ request }) => {
   const response = await request.post(URL, {
     headers: { 'content-type': 'application/json' },
-    data: LoanDto.debtNegative()
-  });
+    data: LoanDto.debtNegative(),
+  })
 
-  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST);
+  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
